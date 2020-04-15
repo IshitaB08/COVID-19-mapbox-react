de60861ffb566dfddf428f2c7dfb8ffc68ddd4c5
IshitaB08 <f20180306@pilani.bits-pilani.ac.in>
2020-04-15 06:39:23 +0530

    indicators added

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ function App() {
     if (data) {
     const map = new mapboxgl.Map({
       container: mapboxElRef.current,
-      style: "mapbox://styles/ishita08/ck8z4r9xd028u1imnxcn6mn8t",
+      style: "mapbox://styles/notalemesa/ck8dqwdum09ju1ioj65e3ql3k",
       center: [75, 27],
       zoom: 2
     });
@@ -64,11 +64,85 @@ function App() {
         type: "circle",
         paint: {
           "circle-opacity": 0.75,
-          "circle-stroke-width": 1,
-          "circle-radius": 4,
-          "circle-color": "#FFEB3B"
+          "circle-stroke-width": [
+               "interpolate",
+               ["linear"],
+               ["get", "cases"],
+               1, 1,
+               100000, 1.75,
+          ],
+          "circle-radius": [
+                "interpolate",
+                 ["linear"],
+                 ["get", "cases"],
+                 1, 4,
+                 1000, 8,
+                 4000, 10,
+                 8000, 14,
+                 12000, 18,
+                 100000, 40
+               ],
+                  "circle-color": [
+                     "interpolate",
+                     ["linear"],
+                     ["get", "cases"],
+                     1, '#ffffb2',
+                     5000, '#fed976',
+                     10000, '#feb24c',
+                     25000, '#fd8d3c',
+                     50000, '#fc4e2a',
+                     75000, '#e31a1c',
+                     100000, '#b10026'
+                   ],
         }
       });
+
+      const popup = new mapboxgl.Popup({
+        closeButton: false,
+        closeOnClick: false
+      });
+
+      let lastId;
+
+      map.on('mousemove', 'circles', (e) => {
+        const id = e.features[0].properties.id;
+
+        if (id !== lastId) {
+          lastId = id;
+          const { cases, deaths, country, province } = e.features[0].properties;
+
+          // Change the pointer type on mouseenter
+          map.getCanvas().style.cursor = 'pointer';
+
+          const coordinates = e.features[0].geometry.coordinates.slice();
+
+          const countryISO = lookup.byCountry(country)?.iso2 || lookup.byInternet(country)?.iso2;
+          const provinceHTML = province !== 'null' ? `<p>Province: <b>${province}</b></p>` : '';
+          const mortalityRate = ((deaths / cases) * 100).toFixed(2);
+          const countryFlagHTML = Boolean(countryISO)
+            ? `<img src="https://www.countryflags.io/${countryISO}/flat/64.png"></img>`
+            : '';
+
+          const HTML = `<p>Country: <b>${country}</b></p>
+            ${provinceHTML}
+            <p>Cases: <b>${cases}</b></p>
+            <p>Deaths: <b>${deaths}</b></p>
+            <p>Mortality Rate: <b>${mortalityRate}%</b></p>
+            ${countryFlagHTML}`;
+
+            while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+              coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+            }
+
+            popup.setLngLat(coordinates).setHTML(HTML).addTo(map);
+          }
+        });
+
+        map.on('mouseleave', 'circles', function () {
+          lastId = undefined;
+          map.getCanvas().style.cursor = '';
+          popup.remove();
+        });
     });
    }
   }, [data]);
